b8b2564559e32673499384909c6d362d532398bd
Brandon Knutson <brkn8102@colorado.edu>
2018-10-06 02:17:33 -0600

    Added multiple balls and ball-on-ball collision.

--- ball.py
+++ ball.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import linalg as LA
+from numpy import linalg as la
 
 class Ball:
 	def __init__(self, m, r, x, y, vx, vy):
@@ -10,8 +10,31 @@ class Ball:
 		self.vx = vx
 		self.vy = vy
 
+	def pos(self):
+		return np.array([self.x, self.y])
+
+	def vel(self):
+		return np.array([self.vx, self.vy])
+
 	def move(self):
 		self.x += self.vx
 		self.y += self.vy
 
-isBouncing(b1, b2)
+def collide(b1, b2):
+	n = b2.pos() - b1.pos()
+	n = n/la.norm(n)
+
+	m = np.array([n[1], -n[0]])
+
+	mu = b2.m/b1.m
+
+	u = b1.vel()-b2.vel()
+	uPer = np.dot(u,n)
+	uPar = np.dot(u,m)
+
+	v = uPer*m + (1-mu)/(1+mu)*uPar*n
+
+	w = 2/(1+mu)*uPar*n
+
+	[b1.vx, b1.vy] = np.rint(v + b2.vel()).astype(int)
+	[b2.vx, b2.vy] = np.rint(w + b2.vel()).astype(int)
--- elastic_balls.py
+++ elastic_balls.py
@@ -1,29 +1,44 @@
-import sys, pygame as pg, ball
+import sys, pygame as pg, numpy as np, ball
+from numpy import linalg as la
+from itertools import combinations
 pg.init()
 
 width, height = 1280, 750
 black = 0, 0, 0
 white = 255, 255, 255
 
-b = ball.Ball(1, 10, width//2, height//2, 10, 10)
+ball1 = ball.Ball(1, 50, width//2, height//2, 10, 10)
+ball2 = ball.Ball(2, 100, 3*width//4, 3*height//4, -10, 10)
+ball3 = ball.Ball(3, 150, width//4, height//4, -10, 10)
+
+balls = [ball1, ball2, ball3]
 
 screen = pg.display.set_mode([width, height])
 pg.display.set_caption("Elastic Balls")
 
 while True:
 	for event in pg.event.get():
-		if event.type == pg.QUIT: sys.exit()
+		if event.type == pg.QUIT:
+			sys.exit()
 
-	b.move()
+	for b in balls:
+		b.move()
 
-	# handle wall collision
-	if b.x - b.r < 0 or width < b.x + b.r:
-		b.vx = -b.vx
-	if b.y - b.r < 0 or height < b.y + b.r:
-		b.vy = -b.vy
+	# wall collision
+	for b in balls:
+		if b.x - b.r < 0 or width < b.x + b.r:
+			b.vx = -b.vx
+		if b.y - b.r < 0 or height < b.y + b.r:
+			b.vy = -b.vy
+
+	# ball collision
+	for (b1, b2) in list(combinations(balls, 2)):
+		if la.norm(b2.pos() - b1.pos()) < b1.r + b2.r:
+			ball.collide(b1,b2)
 
 	screen.fill(white)
-	pg.draw.circle(screen, black, (b.x, b.y), 10)
+	for b in balls:
+		pg.draw.circle(screen, black, (b.x, b.y), b.r)
 	pg.display.flip()
 
 	#pg.time.wait(100)
